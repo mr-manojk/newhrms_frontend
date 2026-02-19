c904542952816b3441bb6ae16cefb5de0d193cae
mr-manojk <>
2026-02-19 23:09:26 +0530

    Hosting error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
   define: {
-    // This ensures 'process.env' is available as a global object in the browser
     'process.env': {
       API_KEY: JSON.stringify(process.env.API_KEY || '')
     },
-    // Polyfill for libraries that expect 'global'
     'global': 'window',
   },
   resolve: {
@@ -22,19 +20,14 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
     proxy: {
-      // Proxy API requests to the local backend during development
       '/api': {
         target: 'https://node-mysql-api-lhbg.onrender.com',
         changeOrigin: true,
-        secure: false,
       },
-      // Proxy uploads to the local backend during development
       '/uploads': {
         target: 'https://node-mysql-api-lhbg.onrender.com',
         changeOrigin: true,
-        secure: false,
       }
     }
   },
@@ -43,13 +36,5 @@ export default defineConfig({
     emptyOutDir: true,
     sourcemap: false,
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
-      },
-      mangle: true,
-    },
   }
 });
